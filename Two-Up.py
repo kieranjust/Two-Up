9ef6bb3e93016cd6182ff30311351500cd2b47e3
Kieran Just <J189778@tafe.wa.edu.au>
2021-03-24 14:14:28 +0800

    First upload of code. Basics only. Scoring system/other modifications to be added

---
+++ Two-Up.py
@@ -0,0 +1,16 @@
+from Game import Game
+
+
+class TwoUp:
+    def __init__(self):
+        self.game = Game()
+
+    def main_function(self):
+        self.game.make_guess()
+        self.game.flip_coin_1()
+        self.game.flip_coin_2()
+        self.game.check_guess()
+
+
+twoUp = TwoUp()
+twoUp.main_function()
