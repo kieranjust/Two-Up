14e4dd7881d8b126607b9c28b3b8028821ff5b92
Kieran Just <J189778@tafe.wa.edu.au>
2021-05-05 14:40:24 +0800

    first iteration of the GUI

--- Game.py
+++ Game.py
@@ -8,32 +8,34 @@ class Game:
         self.__outcome = ''
         self.__player_guess = ''
 
-    def make_guess(self):
-        """Asks the player to guess what the coins will land on. Inputs can be 'HH' 'HT' or 'TT case insensitive"""
-        while not (self.__player_guess == 'HH' or self.__player_guess == 'HT' or self.__player_guess == 'TT'):
-            print("What is your guess? ('HH', 'HT' or 'TT')")
-            self.__player_guess = input().upper()
+    def make_guess_heads_heads(self):
+        """Sets the players guess to heads, heads"""
+        self.__player_guess = "HH"
+
+    def make_guess_heads_tails(self):
+        """Sets the players guess to heads, tails"""
+        self.__player_guess = "HT"
+
+    def make_guess_tails_tails(self):
+        """Sets the players guess to tails, tails"""
+        self.__player_guess = "TT"
 
     def flip_coin_1(self):
         """Flips the first coin using the random number generator. 0 == heads, 1 == tails"""
         if random.randint(0, 1) == 0:
             self.__coin1 = "H"
-            print("The first coin landed on heads.")
             return self.__coin1
         else:
             self.__coin1 = "T"
-            print("The first coin landed on tails.")
             return self.__coin1
 
     def flip_coin_2(self):
         """Flips the second coin using the random number generator. 0 == heads, 1 == tails"""
         if random.randint(0, 1) == 0:
             self.__coin2 = "H"
-            print("The second coin landed on heads.")
             return self.__coin2
         else:
             self.__coin2 = "T"
-            print("The second coin landed on tails.")
             return self.__coin2
 
     def check_guess(self):
@@ -44,7 +46,6 @@ class Game:
             self.__outcome = 'HT'
 
         if self.__outcome == self.__player_guess:
-            print("Congratulations, you guessed correctly!")
+            return"Congratulations, you guessed correctly!"
         else:
-            print("Sorry your guess was incorrect!")
-
+            return"Sorry your guess was incorrect!"
