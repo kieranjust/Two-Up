9c78c1b584955dd3a0d3e06a6c972e4ec48b7e69
VusheshDookheea <69453459+VusheshDookheea@users.noreply.github.com>
2021-05-26 14:12:19 +0800

    Solved issues (#12)
    * tried adding score
    
    * Added Score system
    
    * Added score system
    
    * Update Game.py
    
    * Update Game.py
    
    * made a small changes for my resubmission
    
    * Add new changes to the gui

--- Two-Up.py
+++ Two-Up.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter as tk
 from breezypythongui import EasyFrame
 from Game import Game
 game = Game()
@@ -16,21 +17,25 @@ class TwoUp(EasyFrame):
         self.addLabel(text="Make your bet by clicking one of the three buttons and then clicking the check guess button"
                            ".",
                       row=0, column=0, columnspan=2)
+
+
         button_panel = self.addPanel(row=1, column=0, columnspan=2)
         button_panel.addButton(text="HH", row=0, column=0, command=self.heads_heads_handler)
         button_panel.addButton(text="HT", row=0, column=1, command=self.heads_tails_handler)
         button_panel.addButton(text="TT", row=0, column=2, command=self.tails_tails_handler)
         # Having issues centering the text fields with the row and column counts. Had to stretch with E + W
-        self.guessField = self.addTextField(text="", row=2, column=0, sticky=E + W)
+        self.guessField = self.addTextField(text="", row=2, column=0, sticky=N + S, state='disabled')
+
         self.addButton(text="Check guess",
                        row=3, column=0,
                        command=self.check_guess_handler)
         # Having issues centering the text fields with the row and column counts. Had to stretch with E + W
-        self.outputField1 = self.addTextField(text="", row=4, column=0, sticky=E + W)
+        # Fixed issues where textfield is now set to read only
+        self.outputField1 = self.addTextField(text="", row=4, column=0, sticky=N + S, state='disabled')
         # Having issues centering the text fields with the row and column counts. Had to stretch with E + W
-        self.outputField2 = self.addTextField(text="", row=5, column=0, sticky=E + W)
+        self.outputField2 = self.addTextField(text="", row=5, column=0, sticky=N + S, state='disabled')
         # Having issues centering the text fields with the row and column counts. Had to stretch with E + W
-        self.outputField3 = self.addTextField(text="", row=6, column=0, sticky=E + W)
+        self.outputField3 = self.addTextField(text="", row=6, column=0, sticky=E + W, state='disabled')
 
     def heads_heads_handler(self):
         game.make_guess_heads_heads()
@@ -60,5 +65,3 @@ class TwoUp(EasyFrame):
 
 
 twoUp = TwoUp().mainloop()
-
-
